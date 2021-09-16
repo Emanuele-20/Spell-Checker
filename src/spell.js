84657f39ca7bb55db89960e8128ee1ba173fde0f
Emanuele Pace <emanuele.10@outlook.it>
2021-09-16 10:53:17 +0100

    Managed a checker of a single word, correct and incorrect, without any harcoded code

--- src/spell.js
+++ src/spell.js
@@ -1,14 +1,26 @@
 class Spell {
 	constructor() {
-		this.vocabulary = ["Hi"];
+		this.vocabulary = ["Hi", "Hello", "You"];
 	}
 
 	spellChecker(sentence) {
-		if (this.vocabulary.includes(sentence)) {
-			return sentence;
-		} else {
-			return `~${sentence}~`;
-		}
+		let sentenceContainer = sentence.split(" ");
+		let checkedSentence = [];
+
+		sentenceContainer.forEach((word) => {
+			if (this.vocabulary.includes(word)) {
+				checkedSentence.push(word);
+			} else {
+				checkedSentence.push(`~${word}~`);
+			}
+		});
+		console.log(checkedSentence.join(""));
+		return checkedSentence.join();
+		// if (this.vocabulary.includes(sentenceContainer)) {
+		// 	return sentence;
+		// } else {
+		// 	return `~${sentence}~`;
+		// }
 	}
 }
 
